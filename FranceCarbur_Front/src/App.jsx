2004b3fdbc3f2c48440c443de50a37cdb1f117c5
Victor <victor.moreau1906@gmail.com>
2024-09-27 10:40:18 +0200

    Edit jsx

--- FranceCarbur_Front/src/App.jsx
+++ FranceCarbur_Front/src/App.jsx
@@ -1,48 +1,47 @@
-
-import './App.css'
+import './style.css'
 
 function App() {
 
-  return (      class App extends React.Component{
-      render(){
-          return(
-              <div className="container">
-                  <div className="form-box">
-                      <div className="header-form">
-                          <h4 className="text-primary text-center">
-                              <i className="fa fa-user-circle" style={{fontSize:"110px"}}></i>
-                          </h4>
-                          <div className="image"></div>
-                      </div>
-                      <div className="body-form">
-                          <form>
-                              <div className="input-group mb-3">
-                                  <div className="input-group-prepend">
-                                      <span className="input-group-text"><i className="fa fa-user"></i></span>
-                                  </div>
-                                  <input type="text" className="form-control" placeholder="Identitfiant" />
-                              </div>
-                              <div className="input-group mb-3">
-                                  <div className="input-group-prepend">
-                                      <span className="input-group-text"><i className="fa fa-lock"></i></span>
-                                  </div>
-                                  <input type="password" className="form-control" placeholder="Mot de passe" />
-                              </div>
-                              <button type="button" className="btn btn-secondary btn-block">Se connecter</button>
-                              <button type="button" className="btn btn-secondary btn-block">Connexion invité</button>
-                              <div className="message">
-                              </div>
-                          </form>
-                      </div>
-                  </div>
-              </div>
-          )
-      }
-  }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(new FormData(event.target))
+        //ajout du code pour envoyer un post au back python
+    }
 
-    ReactDOM.render(<App />, document.getElementById('root'));
-
-)
+    return (
+        <>
+            <div className="container">
+                <div className="form-box">
+                    <div className="header-form">
+                        <h4 className="text-primary text-center">
+                            <i className="fa fa-user-circle" style={{fontSize: "110px"}}></i>
+                        </h4>
+                        <div className="image"></div>
+                    </div>
+                    <div className="body-form">
+                        <form onSubmit={handleSubmit}>
+                            <div className="input-group mb-3">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text"><i className="fa fa-user"></i></span>
+                                </div>
+                                <input type="text" className="form-control" name="id" placeholder="Identitfiant"/>
+                            </div>
+                            <div className="input-group mb-3">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text"><i className="fa fa-lock"></i></span>
+                                </div>
+                                <input type="password" className="form-control" name="mdp" placeholder="Mot de passe"/>
+                            </div>
+                            <div>
+                                <button type="submit">Se connecter</button>
+                                <button type="button">Mode invité</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
