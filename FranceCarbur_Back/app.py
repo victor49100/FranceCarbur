ab806eca727f27f06bf896cdbfbabd456115339f
Victor <victor.moreau1906@gmail.com>
2024-09-27 14:16:45 +0200

    ajout changement mdp

--- FranceCarbur_Back/app.py
+++ FranceCarbur_Back/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import psycopg2
 from psycopg2 import OperationalError
 from flask import Flask, render_template
@@ -20,50 +20,100 @@ DATABASE_CONFIG = {
     'sslmode': os.getenv('DB_SSLMODE')
 }
 
+
+# fonction bdd
+
 def fetch_user_by_id(user_id):
     try:
         # Connexion à la base de données
         conn = psycopg2.connect(**DATABASE_CONFIG)
         cur = conn.cursor()
-        
         # Requête SQL pour récupérer les données de la table "user" en fonction de l'ID
         cur.execute('SELECT * FROM public."user" WHERE id_user = %s;', (user_id,))
-        
         # Récupération de la ligne
         user = cur.fetchone()
-        
         # Fermeture du curseur et de la connexion
         cur.close()
         conn.close()
-        
+
         return user
-    
+
     except OperationalError as e:
         return {'error': f'Erreur de connexion : {e}'}
     except Exception as e:
         return {'error': f'Une erreur s\'est produite : {e}'}
+
+
+def update_newPassword_by_id(id, newPassword):
+    try:
+        # Connexion à la base de données
+        conn = psycopg2.connect(**DATABASE_CONFIG)
+        cur = conn.cursor()
+        # Exécution de la requête de mise à jour
+        cur.execute('UPDATE public."user" SET password = %s WHERE id_user = %s;', (newPassword, id))
+        
+        # Commit des modifications
+        conn.commit()
+        # Fermeture des curseurs et connexion
+        cur.close()
+        conn.close()
+        return 0
+    except OperationalError as e:
+        return {'error': f'Erreur de connexion : {e}'}
+    except Exception as e:
+        return {'error': f'Une erreur s\'est produite : {e}'}
+
+
+# ROUTES
+
+# GET
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('home.html')
 
 
-
-@app.route('/app/api/user/<int:user_id>', methods=['GET'])
-def get_user(user_id):
+@app.route('/api/user', methods=['GET'])
+def get_user():
+    args = request.args
+    user_id = args.get('user_id')
     user = fetch_user_by_id(user_id)
-    
+
     if user is None:
         return jsonify({'error': 'Utilisateur non trouvé'}), 404
-    
+
     # Convertir le tuple en un dictionnaire (si nécessaire)
     user_data = {
         'id_user': user[0],
-        'username': user[1],
-        'email': user[2]
+        'first_name': user[1],
+        'last_name': user[2],
+        'pseudo': user[3],
+        'car_type': user[4],
+        'conso': user[5],
+        'car_name': user[6],
+        'password': user[7],
     }
-    
+
     return jsonify(user_data)
+
+
+# POST
+
+
+# PUT
+@app.route('/api/user/updateMDP', methods=['GET', 'PUT'])
+def update_password():
+    args = request.args
+    user_id = args.get('user_id')
+    new_password = args.get('new_password')
+    if request.method == 'PUT':
+        update = update_newPassword_by_id(user_id, new_password)
+        if isinstance(update, dict) and 'error' in update:
+            return jsonify(update), 404
+        return jsonify({'OK': 'Changement réussi'})
+    return jsonify({'error': 'Méthode non autorisée'}), 405
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
